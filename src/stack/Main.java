5224bcf9747b0604ae810c17f9332df7c602aa0e
Alexandre Terrasa <alexandre@moz-code.org>
2016-04-04 18:17:48 -0400

    stack: introduce Stack implementation
    Signed-off-by: Alexandre Terrasa <alexandre@moz-code.org>

---
+++ src/stack/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 Alexandre Terrasa <alexandre@moz-code.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("New Stack");
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        System.out.println("Stack contains " + stack.size() + " elements");
+
+        while (!stack.isEmpty()) {
+            int i = stack.pop();
+            System.out.println(i);
+        }
+        System.out.println("Stack is empty: " + stack.isEmpty());
+    }
+}
